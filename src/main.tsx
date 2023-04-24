abaa2909d3e349fdf23fccdb69bcdb48a653df70
David Oberacker <oberacker@fzi.de>
2023-04-24 16:17:55 +0000

    Create initial version of version 2.0

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./main.scss";
+import '@fortawesome/fontawesome-free/js/fontawesome';
+import '@fortawesome/fontawesome-free/js/solid';
+
+
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
