b9bfb341e19cc22e7224ce928a39f9e85b4bc8ef
Doomse <Hoefer@fzi.de>
2025-03-12 15:58:14 +0100

    Update project configs, mainly eslint

---
+++ eslint.config.mjs
@@ -0,0 +1,33 @@
+import eslint from '@eslint/js';
+import eslintConfigPrettier from 'eslint-config-prettier';
+import eslintPluginVue from 'eslint-plugin-vue';
+import globals from 'globals';
+import typescriptEslint from 'typescript-eslint';
+
+export default typescriptEslint.config(
+
+    {
+        extends: [
+            eslint.configs.recommended,
+            ...typescriptEslint.configs.recommended,
+            ...eslintPluginVue.configs['flat/recommended'],
+        ],
+
+        files: ['src/**/*.{ts,vue}'],
+
+        languageOptions: {
+            ecmaVersion: "latest",
+            sourceType: "script",
+            globals: globals.browser,
+            parserOptions: {
+                parser: typescriptEslint.parser,
+            },
+        },
+
+        rules: {
+            "no-fallthrough": "warn",
+            "no-inner-declarations": "warn",
+        },
+    },
+    eslintConfigPrettier
+);
